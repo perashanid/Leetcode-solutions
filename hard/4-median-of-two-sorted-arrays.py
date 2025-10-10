17f341fbedda01a99dc9ca4d1870a3a87d2be934
Shanid Sajjatuz Islam <shanidsajjatuz@gmail.com>
2025-10-10 21:09:36 +0000

    Add solution: Median of Two Sorted Arrays (Hard)

--- hard/4-median-of-two-sorted-arrays.py
+++ hard/4-median-of-two-sorted-arrays.py
@@ -6,8 +6,8 @@ Link: https://leetcode.com/problems/median-of-two-sorted-arrays/
 Date: 2025-10-10
 """
 
-# Time Complexity: O(log(min(m, n))), where m and n are the lengths of the input arrays.
-# Space Complexity: O(1)
+# Time Complexity: O(log(min(m, n))), where m and n are the lengths of nums1 and nums2 respectively. This is due to the binary search approach.
+# Space Complexity: O(1).  We are using a constant amount of extra space.
 
 class Solution:
     def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
@@ -22,7 +22,7 @@ class Solution:
             The median of the combined sorted arrays.
         """
 
-        # Ensure nums1 is the smaller array to optimize binary search.
+        # Ensure nums1 is the shorter array to optimize binary search
         if len(nums1) > len(nums2):
             nums1, nums2 = nums2, nums1
 
@@ -33,8 +33,6 @@ class Solution:
             partitionX = (low + high) // 2
             partitionY = (m + n + 1) // 2 - partitionX
 
-            # If partitionX is 0, then nothing is there on the left side. Use -infinity
-            # If partitionX is m, then nothing is there on the right side. Use +infinity
             maxLeftX = nums1[partitionX - 1] if partitionX > 0 else float('-inf')
             minRightX = nums1[partitionX] if partitionX < m else float('inf')
 
@@ -42,15 +40,16 @@ class Solution:
             minRightY = nums2[partitionY] if partitionY < n else float('inf')
 
             if maxLeftX <= minRightY and maxLeftY <= minRightX:
-                # We have found the right partition.
-                # Now, compute the median based on even/odd length.
+                # We have found the correct partition
                 if (m + n) % 2 == 0:
+                    # Even number of elements
                     return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2
                 else:
+                    # Odd number of elements
                     return max(maxLeftX, maxLeftY)
             elif maxLeftX > minRightY:
-                # We are too far on the right side for nums1. Go to the left.
+                # Move towards the left in nums1
                 high = partitionX - 1
             else:
-                # We are too far on the left side for nums1. Go to the right.
+                # Move towards the right in nums1
                 low = partitionX + 1
